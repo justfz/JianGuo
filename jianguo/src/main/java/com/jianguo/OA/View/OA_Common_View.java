38e587362def244cde0a6fe83403d1531daad907
Justonezf <justonezf@outlook.com>
2016-11-26 19:43:00 +0800

    剔除友盟推送

---
+++ jianguo/src/main/java/com/jianguo/OA/View/OA_Common_View.java
@@ -0,0 +1,12 @@
+package com.jianguo.OA.View;
+
+import com.jianguo.beans.NewsBean;
+
+import java.util.List;
+
+/**
+ * Created by ifane on 2016/8/27 0027.
+ */
+public interface OA_Common_View {
+    void setDataBean(List<NewsBean> newsBeen);
+}
